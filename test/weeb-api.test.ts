db2d12674a3e0f8a4f978376e9f1be0693d9b4cd
Mei <xanizl@me.com>
2017-08-28 18:57:02 -0400

    Update tests and setup travis

--- test/weeb-api.test.ts
+++ test/weeb-api.test.ts
@@ -1,63 +1,69 @@
 import WeebAPI from "../src/weeb-api"
 
-const weebClient = new WeebAPI(process.env.TOKEN)
+const weebApi = new WeebAPI(process.env.TOKEN);
 
 describe("Get Types Array", () => {
   it("got data from api", () => {
-    weebClient
-      .getTypes()
-      .then(data => expect(data).toHaveProperty("types"))
-      .catch(e => expect(e).toBeDefined())
-  })
-})
+    weebApi.getTypes().then(data => {
+        expect(data).toHaveProperty("types")
+      }).catch(e => {
+        expect(e).toBeDefined()
+      });
+  });
+});
 
 describe("Get Tags Array", () => {
   it("got data from api", () => {
-    weebClient
-      .getTags()
-      .then(data => expect(data).toHaveProperty("tags"))
-      .catch(e => expect(e).toBeDefined())
-  })
-})
+    weebApi.getTags().then(data => {
+        expect(data).toHaveProperty("tags");
+    }).catch(e => {
+      expect(e).toBeDefined();
+    });
+  });
+});
 
 describe("Get Image by ID", () => {
   it("got data from api", () => {
-    weebClient
-      .getImage("Hko6RAPFx")
-      .then(data => expect(data).toHaveProperty("type"))
-      .catch(e => expect(e).toBeDefined())
-  })
-})
+    weebApi.getImage("Hko6RAPFx").then(data => {
+      expect(data).toHaveProperty("type")
+    }).catch(e => {
+      expect(e).toBeDefined()
+    });
+  });
+});
 
 describe("Get Random Image", () => {
   it("got data from api with types", () => {
-    weebClient
-      .getRandom({
-        type: "kiss",
-        nsfw: "only"
-      })
-      .then(data => expect(data).toHaveProperty("type"))
-      .catch(e => expect(e).toBeDefined())
-  })
+    weebApi.getRandom({
+      type: "kiss",
+      nsfw: "only"
+    }).then(data => {
+      expect(data).toHaveProperty("type")
+    }).catch(e => {
+      expect(e).toBeDefined()
+    });
+  });
 
   it("got data from api with tags", () => {
-    weebClient
-      .getRandom({
-        tags: "girl",
-        nsfw: "true"
-      })
-      .then(data => expect(data).toHaveProperty("type"))
-      .catch(e => expect(e).toBeDefined())
-  })
+    weebApi.getRandom({
+      tags: "girl",
+      nsfw: "true"
+    }).then(data => {
+      expect(data).toHaveProperty("type")
+    }).catch(e => {
+      expect(e).toBeDefined()
+    });
+  });
 
   it("got data from api with both", () => {
-    weebClient
-      .getRandom({
-        type: "hug",
-        tags: "girl",
-        nsfw: "false"
-      })
-      .then(data => expect(data).toHaveProperty("type"))
-      .catch(e => expect(e).toBeDefined())
-  })
-})
+    weebApi.getRandom({
+      type: "hug",
+      tags: "girl",
+      nsfw: "false"
+    }).then(data => {
+      expect(data).toHaveProperty("type")
+    }).catch(e => {
+      expect(e).toBeDefined()
+    });
+  });
+});
